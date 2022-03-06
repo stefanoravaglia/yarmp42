0f9dcecffa1130c9bb25bdf329643a577f91fec0
stefanoravaglia <89441092+stefanoravaglia@users.noreply.github.com>
2022-03-06 18:42:03 +0100

    02a_sample.py
    notebook export as  python script

---
+++ 02a_sample.py
@@ -0,0 +1,15 @@
+# %% [markdown]
+# # hello wolf
+# tests if environment is installed
+#
+# loads and runs as a ipykernelized jupyter notebook
+# %%
+print('hello wolf')
+
+# %%
+# pep 20
+import this
+
+
+
+# %%
